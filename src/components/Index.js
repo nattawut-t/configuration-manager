6cada266bcabf74b8c5ead4593f55e9c4bbed919
Nattawut T <nattawut.t@kbtg.tech>
2018-01-04 11:13:21 +0700

    update Index component

--- src/components/Index.js
+++ src/components/Index.js
@@ -4,26 +4,29 @@ import Alert from 'react-s-alert'
 
 import NotFound from '../components/errors/NotFound'
 import InternalServerError from '../components/errors/InternalServerError'
-// import UrlRecover from '../containers/UrlRecover'
-// import Email from '../containers/Email'
-import Peer from '../containers/configs/Peer'
-import Profile from '../containers/configs/Profile'
-import Api from '../containers/configs/Api'
+
+import Peer from '../containers/configurationManagements/Peer'
+import Chaincode from '../containers/configurationManagements/Chaincode'
+import Database from '../containers/configurationManagements/Database'
+import Api from '../containers/configurationManagements/Api'
 
 import Login from '../containers/Login'
 import Layout from '../containers/Layout'
+import Wizard from './wizards/Index'
 
 const Index = () =>
   <div>
     <HashRouter>
       <Switch>
+        <Route exact path="/wizard" name="Login" component={Wizard} />
         <Route exact path="/login" name="Login" component={Login} />
         <Route exact path="/configs/peer" name="Peer" render={() => <Layout><Peer /></Layout>} />
-        <Route exact path="/configs/profile" name="Profile" render={() => <Layout><Profile /></Layout>} />
+        <Route exact path="/configs/chaincode" name="Peer" render={() => <Layout><Chaincode /></Layout>} />
+        <Route exact path="/configs/database" name="Profile" render={() => <Layout><Database /></Layout>} />
         <Route exact path="/configs/api" name="Recovery" render={() => <Layout><Api /></Layout>} />
         <Route exact path="/404" name="NotFound" component={NotFound} />
         <Route exact path="/500" name="InternalServerError" component={InternalServerError} />
-        <Redirect from="/" to="/login" />
+        <Redirect from="/" to="/wizard" />
         <Route component={NotFound} />
       </Switch>
     </HashRouter>
---
+++ src/components/wizards/Index.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react'
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from 'reactstrap'
+import { Icon } from 'react-fa'
+
+import Peer from '../../containers/configurationManagements/Peer'
+import Chaincode from '../../containers/configurationManagements/Chaincode'
+import Database from '../../containers/configurationManagements/Database'
+import Api from '../../containers/configurationManagements/Api'
+
+const map = {
+  1: () => <Peer />,
+  2: () => <Chaincode />,
+  3: () => <Database />,
+  4: () => <Api />,
+}
+const firstStep = 1
+const lastStep = 4
+
+class Index extends Component {
+  state = {
+    modal: false,
+    step: 1,
+    CurrentComponent: () => <Peer />,
+  }
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal,
+    })
+  }
+
+  cancel = () => { }
+
+  back = step => {
+    if (step === firstStep) return false
+
+    console.log('step', step)
+    const prevStep = --step
+    console.log('nextStep', prevStep)
+    const CurrentComponent = map[prevStep]
+
+    return this.setState({
+      step: prevStep,
+      CurrentComponent: () => <CurrentComponent />,
+    })
+  }
+
+  next = step => {
+    if (step === lastStep) return false
+
+    console.log('step', step)
+    const nextStep = ++step
+    console.log('nextStep', nextStep)
+    const CurrentComponent = map[nextStep]
+
+    return this.setState({
+      step: nextStep,
+      CurrentComponent: () => <CurrentComponent />,
+    })
+  }
+
+  render() {
+    const { loading } = this.props
+    const { step, CurrentComponent } = this.state
+
+    return (
+      <div>
+        <Modal
+          isOpen
+          toggle={this.toggle}
+          className={this.props.className}
+        >
+          <ModalHeader toggle={this.toggle}>Configuration Wizard</ModalHeader>
+          <ModalBody>
+            <CurrentComponent />
+          </ModalBody>
+          <ModalFooter>
+            {/* <Button color="secondary" onClick={this.toggle}>Back</Button>{' '}
+            <Button color="primary" onClick={this.toggle}>Next</Button> */}
+
+            {step > firstStep ?
+              <Button
+                name="back"
+                color="primary"
+                className="px-4"
+                onClick={() => this.back(step)}
+              >
+                <Icon spin name={loading ? 'circle-o-notch' : ''} size="lg" />&nbsp;&nbsp;Back
+              </Button> : ''
+            }
+
+            {loading ?
+              <Button
+                className="px-4"
+                onClick={this.cancel}
+              >
+                Cancel
+              </Button> : ''
+            }
+
+            &nbsp;&nbsp;
+            <Button
+              name="next"
+              color="primary"
+              className="px-4"
+              onClick={() => this.next(step)}
+            >
+              <Icon spin name={loading ? 'circle-o-notch' : ''} size="lg" />&nbsp;&nbsp; {step === lastStep ? 'Finish' : 'Next'}
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+export default Index
